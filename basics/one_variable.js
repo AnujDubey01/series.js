a2078a7250996c2ebf25347bc6b27786fa4bd9e0
Anuj Dubey <149352591+AnujDubey01@users.noreply.github.com>
2025-02-21 10:11:51 +0000

    dataType

---
+++ basics/one_variable.js
@@ -0,0 +1,13 @@
+const acc_Id = 1240214
+let acc_Holder = "Anuj Dubey"
+var acc_Bank = "SBI" 
+let acc_State ; // when we declare a keyword to a variable but do not assign the value to it the output of that variable is undfined //
+console.log(acc_Id)
+
+//  acc_Id = 1234 --> cannot be changes as the Const Keyword is used  //
+
+acc_Holder = "Anuj"   
+acc_Bank = "RBI"    // since var keyword in not used these days due to its issue related to the scope we opnly use let variable //
+
+console.table([acc_Id , acc_Bank , acc_Holder , acc_State])  // table keywor allows us to pleint all the variable in the teabular form //
+
